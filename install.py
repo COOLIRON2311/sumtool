ea6afe9fcd2ef6840edfe2b4e0c573bcf64247b5
Ivan Ignatenko <31288825+COOLIRON2311@users.noreply.github.com>
2018-04-15 14:48:05 +0300

    Add files via upload

---
+++ install.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import time
+import sys
+
+
+if int(sys.version[:1]) < 3:
+    def input(prompt=''):
+        return raw_input(str(prompt))
+
+
+def out(prompt):
+    sys.stdout.write(prompt)
+    sys.stdout.flush()
+
+
+def reline():
+    sys.stdout.write('\r')
+    sys.stdout.flush()
+
+
+out('Checking os type...')
+time.sleep(1)
+reline()
+sys.stdout.write(str('Checking os type... ') + os.name + '\n')
+sys.stdout.flush()
+time.sleep(1)
+
+
+if os.name == 'posix':
+    out('Checking package structure...')
+    if os.path.isfile('./sumtool') or os.path.isfile('./sumtool.py'):
+        if os.path.isfile('./sumtool.py'):
+            os.rename('./sumtool.py', './sumtool')
+        time.sleep(1)
+        reline()
+        out('Checking package structure... OK\n')
+        time.sleep(1)
+    else:
+        time.sleep(1)
+        reline()
+        out('Checking package structure... Done\n')
+        time.sleep(1)
+        print("Can't find \033[4msumtool\033[0m file in current dir.\n"
+              'The package is either damaged or already installed.')
+        exit()
+
+    out('Starting posix install...')
+    time.sleep(1)
+    reline()
+    out('Starting posix install... OK\n')
+    time.sleep(1)
+    print('\nWhere to install?\n')
+    print('1). /usr/local/bin')
+    print('2). /usr/bin')
+    print('3). /bin')
+    print('4). Exit\n')
+    x = input('Choice: ')
+    if x == '1':
+        time.sleep(1)
+        out('Installing to /usr/local/bin/...')
+        os.system('chmod +x sumtool')
+        try:
+            os.rename('./sumtool', '/usr/local/bin/sumtool')
+            time.sleep(1)
+            reline()
+            out('Installing to /usr/local/bin/... OK\n')
+        except PermissionError:
+            time.sleep(1)
+            reline()
+            out('Installing to /usr/local/bin/... PermissionError\n')
+            time.sleep(1)
+            print('Trying with sudo...')
+            os.system('sudo mv ./sumtool /usr/local/bin/')
+            if not os.path.isfile('/usr/local/bin/sumtool'):
+                print('\n-------------------\ninstallation failed\n-------------------\n')
+                exit()
+    elif x == '2':
+        time.sleep(1)
+        out('Installing to /usr/bin/...')
+        os.system('chmod +x sumtool')
+        try:
+            os.rename('./sumtool', '/usr/bin/sumtool')
+            time.sleep(1)
+            reline()
+            out('Installing to /usr/bin/... OK\n')
+        except PermissionError:
+            time.sleep(1)
+            reline()
+            out('Installing to /usr/bin/... PermissionError\n')
+            time.sleep(1)
+            print('Trying with sudo...')
+            os.system('sudo mv ./sumtool /usr/bin/')
+            if not os.path.isfile('/usr/bin/sumtool'):
+                print('\n-------------------\ninstallation failed\n-------------------\n')
+                exit()
+    elif x == '3':
+        time.sleep(1)
+        out('Installing to /bin/...')
+        os.system('chmod +x sumtool')
+        try:
+            os.rename('./sumtool', '/bin/sumtool')
+            time.sleep(1)
+            reline()
+            out('Installing to /bin/... OK\n')
+        except PermissionError:
+            time.sleep(1)
+            reline()
+            out('Installing to /bin/... PermissionError\n')
+            time.sleep(1)
+            print('Trying with sudo...')
+            os.system('sudo mv ./sumtool /bin/')
+            if not os.path.isfile('/bin/sumtool'):
+                print('\n-------------------\ninstallation failed\n-------------------\n')
+                exit()
+    else:
+        if x != '4':
+            print('Unknown option.')
+        exit()
+
+
+elif os.name == 'nt':
+    if not os.path.isdir('C:\Program Files\sumtool'):
+        try:
+            os.mkdir('C:\Program Files\sumtool')
+        except PermissionError:
+            if not os.path.isfile('.\install.cmd'):
+                buf = '@echo off\ncd /d %~dp0\npython install.py'
+                f = open('install.cmd', 'w')
+                f.write(buf)
+                f.close()
+                print('You need admin rights for installation!\nYou can do so by running install.cmd as admin.')
+                exit()
+    out('Checking package structure...')
+    
+    if not os.path.isfile('.\sumtool'):
+        time.sleep(1)
+        reline()
+        out('Checking package structure... Done\n')
+        time.sleep(1)
+        print("Can't find sumtool file in current dir.\n"
+              'The package is either damaged or already installed.')
+        exit()
+
+    elif os.path.isfile('.\sumtool'):
+        time.sleep(1)
+        reline()
+        out('Checking package structure... Done\n')
+
+    out('Starting nt install...')
+
+    time.sleep(1)
+    reline()
+    out('Starting nt install... OK\n')
+
+    out('Handling dependencies...')
+
+    buf = '@echo off\npython "C:\Program Files\sumtool\sumtool" %*'
+    f = open('sumtool.cmd', 'w')
+    f.write(buf)
+    f.close()
+
+    time.sleep(1)
+    reline()
+    out('Handling dependencies... OK\n')
+
+    time.sleep(1)
+    out('Installing to C:\Program Files\sumtool')
+
+    if not os.path.isdir('C:\Program Files\sumtool'):
+        os.mkdir('C:\Program Files\sumtool')
+    os.rename('.\sumtool', 'C:\Program Files\sumtool\sumtool')
+    os.rename('.\sumtool.cmd', 'C:\Program Files\sumtool\sumtool.cmd')
+
+    time.sleep(1)
+    reline()
+    out('Installing to C:\Program Files\sumtool... OK\n')
+    time.sleep(1)
+    print('Running PATH configuration to finish setup...\n')
+    time.sleep(2)
+    print('Where to add?\n')
+    print('1). Add to user PATH')
+    print('2). Add to system Path')
+    print('3). Both previous options')
+    print('4). Exit\n')
+    x = input('Choice: ')
+    if x == '1':
+        time.sleep(1)
+        print('Adding to user PATH...')
+        os.system('SETX PATH "%PATH%;C:\Program Files\sumtool"')
+    elif x == '2':
+        time.sleep(1)
+        print('Adding to system Path...')
+        os.system('SETX PATH /M "%PATH%;C:\Program Files\sumtool"')
+    elif x == '3':
+        time.sleep(1)
+        out('Adding to both paths...')
+        os.system('SETX PATH "%PATH%;C:\Program Files\sumtool"')
+        os.system('SETX PATH /M "%PATH%;C:\Program Files\sumtool"')
+    else:
+        if x != '4':
+            print('Unknown option.')
+        exit()
+    time.sleep(1)
+print('\n------------------------------\nsumtool successfully installed\n------------------------------\n')
+
+
---
+++ sumtool.py
@@ -0,0 +1,342 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import hashlib
+import os
+ver_num = '0.3'
+
+
+def showhelp():
+    print('sumtool '+ver_num+' by Ivan Ignatenko\n'
+          '\n'
+          'Usage:\n'
+          'sumtool <mode> -{algorithm} [file1] [file2 or checksum]\n'
+          '\n'
+          '<Modes>:\n'
+          '  c    Comparison mode (Compare two files)\n'
+          '  v    Verification mode (Verify file by checksum)\n'
+          '  g    Generation mode (Generate checksum of file)\n'
+          '\n'
+          '{Algorithms}:\n'
+          '  md5 (Default)\n'
+          '  sha1\n'
+          '  sha224\n'
+          '  sha256\n'
+          '  sha384\n'
+          '  sha512\n')
+
+
+def md5(file):
+    hasher = hashlib.md5()
+    BLOCKSIZE = 65536
+    with open(file, 'rb') as f:
+        while True:
+            data = f.read(BLOCKSIZE)
+            if not data:
+                break
+            hasher.update(data)
+    return hasher.hexdigest()
+
+
+def sha1(file):
+    hasher = hashlib.sha1()
+    BLOCKSIZE = 65536
+    with open(file, 'rb') as f:
+        while True:
+            data = f.read(BLOCKSIZE)
+            if not data:
+                break
+            hasher.update(data)
+    return hasher.hexdigest()
+
+
+def sha224(file):
+    hasher = hashlib.sha224()
+    BLOCKSIZE = 65536
+    with open(file, 'rb') as f:
+        while True:
+            data = f.read(BLOCKSIZE)
+            if not data:
+                break
+            hasher.update(data)
+    return hasher.hexdigest()
+
+
+def sha256(file):
+    hasher = hashlib.sha256()
+    BLOCKSIZE = 65536
+    with open(file, 'rb') as f:
+        while True:
+            data = f.read(BLOCKSIZE)
+            if not data:
+                break
+            hasher.update(data)
+    return hasher.hexdigest()
+
+
+def sha384(file):
+    hasher = hashlib.sha384()
+    BLOCKSIZE = 65536
+    with open(file, 'rb') as f:
+        while True:
+            data = f.read(BLOCKSIZE)
+            if not data:
+                break
+            hasher.update(data)
+    return hasher.hexdigest()
+
+
+def sha512(file):
+    hasher = hashlib.sha512()
+    BLOCKSIZE = 65536
+    with open(file, 'rb') as f:
+        while True:
+            data = f.read(BLOCKSIZE)
+            if not data:
+                break
+            hasher.update(data)
+    return hasher.hexdigest()
+
+
+if len(sys.argv) > 1:
+    if len(sys.argv) == 4:
+        try:
+            if sys.argv[1] == 'c':
+                result1 = md5(sys.argv[2])
+                result2 = md5(sys.argv[3])
+                if result1 == result2:
+                    print('\nChecksums match. Files are identical.\n')
+                    print(str('SUM: ' + result1))
+                    print(' ')
+                else:
+                    print('\nChecksums mismatch found.\n')
+                    print(str('SUM1: ' + result1))
+                    print(str('SUM2: ' + result2))
+                    print(' ')
+            elif sys.argv[1] == 'v':
+                result1 = md5(sys.argv[2])
+                result2 = sys.argv[3]
+                if result1 == result2:
+                    print('\nVerification successful. Checksums match.\n')
+                    print(str('SUM: ' + result1))
+                    print(' ')
+                else:
+                    print('\nVerification successful. Checksums mismatch found.\n')
+                    print(str('SUM1: ' + result1))
+                    print(str('SUM2: ' + result2))
+                    print(' ')
+
+            elif sys.argv[1] == 'g':
+                if sys.argv[2] == '-md5':
+                    result1 = md5(sys.argv[3])
+                    print('\nMD5: ' + result1 + '\n')
+
+                elif sys.argv[2] == '-sha1' or sys.argv[2] == '-sha' or sys.argv[2] == '-1':
+                    result1 = sha1(sys.argv[3])
+                    print('\nSHA1: ' + result1 + '\n')
+
+                elif sys.argv[2] == '-sha224' or sys.argv[2] == '-224':
+                    result1 = sha224(sys.argv[3])
+                    print('\nSHA224: ' + result1 + '\n')
+
+                elif sys.argv[2] == '-sha256' or sys.argv[2] == '-256':
+                    result1 = sha256(sys.argv[3])
+                    print('\nSHA256: ' + result1 + '\n')
+
+                elif sys.argv[2] == '-sha384' or sys.argv[2] == '-384':
+                    result1 = sha384(sys.argv[3])
+                    print('\nSHA384: ' + result1 + '\n')
+
+                elif sys.argv[2] == '-sha512' or sys.argv[2] == '-512':
+                    result1 = sha512(sys.argv[3])
+                    print('\nSHA512: ' + result1 + '\n')
+
+        except IOError:
+            if os.name == 'nt':
+                print('No such file or checksum\n')
+            else:
+                print('\033[4mNo such file or checksum\033[0m\n')
+            showhelp()
+
+    elif len(sys.argv) == 3 and sys.argv[1] == 'g':
+        try:
+            result1 = md5(sys.argv[2])
+            print('\nMD5: ' + result1 + '\n')
+
+        except IOError:
+            if os.name == 'nt':
+                print('No such file or checksum\n')
+            else:
+                print('\033[4mNo such file or checksum\033[0m\n')
+            showhelp()
+
+    elif len(sys.argv) == 5:
+        try:
+            if sys.argv[1] == 'c':
+                if sys.argv[2] == '-md5':
+                    result1 = md5(sys.argv[3])
+                    result2 = md5(sys.argv[4])
+                    if result1 == result2:
+                        print('\nChecksums match. Files are identical.\n')
+                        print(str('SUM: ' + result1))
+                        print(' ')
+                    else:
+                        print('\nChecksums mismatch found.\n')
+                        print(str('SUM1: ' + result1))
+                        print(str('SUM2: ' + result2))
+                        print(' ')
+
+                elif sys.argv[2] == '-sha1' or sys.argv[2] == '-sha' or sys.argv[2] == '-1':
+                    result1 = sha1(sys.argv[3])
+                    result2 = sha1(sys.argv[4])
+                    if result1 == result2:
+                        print('\nChecksums match. Files are identical.\n')
+                        print(str('SUM: ' + result1))
+                        print(' ')
+                    else:
+                        print('\nChecksums mismatch found.\n')
+                        print(str('SUM1: ' + result1))
+                        print(str('SUM2: ' + result2))
+                        print(' ')
+
+                elif sys.argv[2] == '-sha224' or sys.argv[2] == '-224':
+                    result1 = sha224(sys.argv[3])
+                    result2 = sha224(sys.argv[4])
+                    if result1 == result2:
+                        print('\nChecksums match. Files are identical.\n')
+                        print(str('SUM: ' + result1))
+                        print(' ')
+                    else:
+                        print('\nChecksums mismatch found.\n')
+                        print(str('SUM1: ' + result1))
+                        print(str('SUM2: ' + result2))
+                        print(' ')
+
+                elif sys.argv[2] == '-sha256' or sys.argv[2] == '-256':
+                    result1 = sha256(sys.argv[3])
+                    result2 = sha256(sys.argv[4])
+                    if result1 == result2:
+                        print('\nChecksums match. Files are indentical.\n')
+                        print(str('SUM: ' + result1))
+                        print(' ')
+                    else:
+                        print('\nChecksums mismatch found.\n')
+                        print(str('SUM1: ' + result1))
+                        print(str('SUM2: ' + result2))
+                        print(' ')
+
+                elif sys.argv[2] == '-sha384' or sys.argv[2] == '-384':
+                    result1 = sha256(sys.argv[3])
+                    result2 = sha256(sys.argv[4])
+                    if result1 == result2:
+                        print('\nChecksums match. Files are identical.\n')
+                        print(str('SUM: ' + result1))
+                        print(' ')
+                    else:
+                        print('\nChecksums mismatch found.\n')
+                        print(str('SUM1: ' + result1))
+                        print(str('SUM2: ' + result2))
+                        print(' ')
+
+                elif sys.argv[2] == '-sha512' or sys.argv[2] == '-512':
+                    result1 = sha512(sys.argv[3])
+                    result2 = sha512(sys.argv[4])
+                    if result1 == result2:
+                        print('\nChecksums match. Files are identical.\n')
+                        print(str('SUM: ' + result1))
+                        print(' ')
+                    else:
+                        print('\nChecksums mismatch found.\n')
+                        print(str('SUM1: ' + result1))
+                        print(str('SUM2: ' + result2))
+                        print(' ')
+
+            elif sys.argv[1] == 'v':
+                if sys.argv[2] == '-md5':
+                    result1 = md5(sys.argv[3])
+                    result2 = sys.argv[4]
+                    if result1 == result2:
+                        print('\nVerification successful. Checksums match.\n')
+                        print(str('SUM: ' + result1))
+                        print(' ')
+                    else:
+                        print('\nVerification successful. Checksums mismatch found.\n')
+                        print(str('SUM1: ' + result1))
+                        print(str('SUM2: ' + result2))
+                        print(' ')
+
+                elif sys.argv[2] == '-sha1' or sys.argv[2] == '-sha' or sys.argv[2] == '-1':
+                    result1 = sha1(sys.argv[3])
+                    result2 = sys.argv[4]
+                    if result1 == result2:
+                        print('\nVerification successful. Checksums match.\n')
+                        print(str('SUM: ' + result1))
+                        print(' ')
+                    else:
+                        print('\nVerification successful. Checksums mismatch found.\n')
+                        print(str('SUM1: ' + result1))
+                        print(str('SUM2: ' + result2))
+                        print(' ')
+
+                elif sys.argv[2] == '-sha224' or sys.argv[2] == '-224':
+                    result1 = sha224(sys.argv[3])
+                    result2 = sys.argv[4]
+                    if result1 == result2:
+                        print('\nVerification successful. Checksums match.\n')
+                        print(str('SUM: ' + result1))
+                        print(' ')
+                    else:
+                        print('\nVerification successful. Checksums mismatch found.\n')
+                        print(str('SUM1: ' + result1))
+                        print(str('SUM2: ' + result2))
+                        print(' ')
+
+                elif sys.argv[2] == '-sha256' or sys.argv[2] == '-256':
+                    result1 = sha256(sys.argv[3])
+                    result2 = sys.argv[4]
+                    if result1 == result2:
+                        print('\nVerification successful. Checksums match.\n')
+                        print(str('SUM: ' + result1))
+                        print(' ')
+                    else:
+                        print('\nVerification successful. Checksums mismatch found.\n')
+                        print(str('SUM1: ' + result1))
+                        print(str('SUM2: ' + result2))
+                        print(' ')
+
+                elif sys.argv[2] == '-sha384' or sys.argv[2] == '-384':
+                    result1 = sha384(sys.argv[3])
+                    result2 = sys.argv[4]
+                    if result1 == result2:
+                        print('\nVerification successful. Checksums match.\n')
+                        print(str('SUM: ' + result1))
+                        print(' ')
+                    else:
+                        print('\nVerification successful. Checksums mismatch found.\n')
+                        print(str('SUM1: ' + result1))
+                        print(str('SUM2: ' + result2))
+                        print(' ')
+
+                elif sys.argv[2] == '-sha512' or sys.argv[2] == '-512':
+                    result1 = sha512(sys.argv[3])
+                    result2 = sys.argv[4]
+                    if result1 == result2:
+                        print('\nVerification successful. Checksums match.\n')
+                        print(str('SUM: ' + result1))
+                        print(' ')
+                    else:
+                        print('\nVerification successful. Checksums mismatch found.\n')
+                        print(str('SUM1: ' + result1))
+                        print(str('SUM2: ' + result2))
+                        print(' ')
+
+        except IOError:
+            if os.name == 'nt':
+                print('No such file or checksum\n')
+            else:
+                print('\033[4mNo such file or checksum\033[0m\n')
+            showhelp()
+    elif 1 < len(sys.argv) < 4:
+        showhelp()
+else:
+    showhelp()
